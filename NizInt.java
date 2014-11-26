7df128850787d399cf7b8785641579e97bb27d45
Gordan Sajević <gsajevic@hotmail.com>
2014-11-26 14:49:31 +0100

    Create NizInt.java

---
+++ NizInt.java
@@ -0,0 +1,26 @@
+package lab12;
+
+public class NizInt {
+
+	public static int[] unosNiza()
+	{
+		int niz []= new int[5];
+		for (int i=0; i<5; i++)
+		{
+			System.out.println("Unesite " + (i+1) +  ". član niza: ");
+			niz[i] = TextIO.getInt();
+		}
+		return niz;
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		int[] array = new int[5];
+		array = unosNiza();
+		for (int i=0; i<5; i++)
+		{
+			System.out.println((i+1) + ". član niza: " + array[i]);
+		}
+	}
+}
